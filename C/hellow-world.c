bf2920a20b4e64a243fa0972a70d73e18c34e392
Alfonso Cantos <alfonso.cantos@gmail.com>
2015-09-23 13:16:59 +0000

    A branch an arguments added.
    Signed-off-by: Alfonso Cantos <alfonso.cantos@gmail.com>

--- C/hellow-world.c
+++ C/hellow-world.c
@@ -1,7 +1,15 @@
 /*Hello World program*/
 
-# include <stdio.h>
+#include <stdio.h>
 
-int main (){
-    printf("hello world!\n");
+#define DEFAULT "World"
+int main (int argc, char *argv[]){
+	
+	char* string;
+	if (argc <= 1){
+    		string = DEFAULT;
+	} else {
+	    	string = argv[1];
+	}
+	printf("hello %s!\n", string);
 }
