0b404c2ef92096a243c7a7014e458342f48adaa2
Alfonso Cantos <alfonso.cantos@gmail.com>
2015-09-23 14:25:54 +0000

    indentation
    Signed-off-by: Alfonso Cantos <alfonso.cantos@gmail.com>

--- C/hellow-world.c
+++ C/hellow-world.c
@@ -6,11 +6,13 @@
 int main (int argc, char *argv[]){
 	
 	char* string;
+	
 	if (argc <= 1){
     		string = DEFAULT;
 	} else {
 	    	string = argv[1];
 	}
+	
 	printf("hello %s!\n", string);
 	return 0;
 }
