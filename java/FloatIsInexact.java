582291c7e6257c1b37ac6aa2a0eea7dd205eeae0
Alfonso Cantos <alfonso.cantos@gmail.com>
2015-09-24 10:34:48 +0000

    Added an example using BigDecimal.
    Signed-off-by: Alfonso Cantos <alfonso.cantos@gmail.com>

--- java/FloatIsInexact.java
+++ java/FloatIsInexact.java
@@ -1,9 +1,17 @@
 /**
+*	Don't use Float for work with money calcularions,
+*	just for measurements, not when precision is a req.
+*
 *   From http://www.odi.ch/prog/design/newbies.php#46
 */
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 
 class FloatIsInexact {
 	public static void main (String args[]){
+		
+		// The wrong way.
 		float total = 0.0f;
 		float addition = 0.30f;
 		for (int i = 0; i<100; i++){
@@ -11,5 +19,14 @@ class FloatIsInexact {
 		}
 		System.out.println("Expecting 0.30 x 100 = 30.0 but... " 
 				+ total);
+
+		// The correct way.
+		BigDecimal totalBD = BigDecimal.ZERO;
+		BigDecimal additionBD = new BigDecimal(0.30f);
+		for (int i = 0; i<100; i++){
+                        totalBD = new BigDecimal(totalBD.add(additionBD).toString());
+                }
+		System.out.println (totalBD.setScale(2, RoundingMode.HALF_UP));
+
 	}
 }
